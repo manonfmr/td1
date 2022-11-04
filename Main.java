0a16af827298c8b95ee72f570f67b0b38af82ad7
manonfmr <117460396+manonfmr@users.noreply.github.com>
2022-11-04 14:39:30 +0000

    ex3

--- Main.java
+++ Main.java
@@ -2,17 +2,27 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Bonjour");
-    System.out.println("Famery");
+    // System.out.println("Bonjour");
+    // System.out.println("Famery");
 
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Taper un nombre");
-    int nombreEntree = scanner.nextInt();
-    for (int i=1; i<11;i++){
-      System.out.println(nombreEntree*i);
+    // Scanner scanner = new Scanner(System.in);
+    // System.out.println("Taper un nombre");
+    // int nombreEntree = scanner.nextInt();
+    // for (int i=1; i<11;i++){
+    //   System.out.println(nombreEntree*i);
+    // }
+    // scanner.close();
+
+    for(int i = 0; i < 15; i++){
+      if(i % 2 == 0 && i < 9){
+        System.out.println(" * * * * * * ========================");
+      }else if(i % 2 != 0 && i < 9){
+        System.out.println("  * * * * *  ========================");
+      }else{
+        System.out.println(" ====================================");
+      }
     }
-    scanner.close();
     
   }
 }
