aac536a4eee5e447b2bc4b7fabc965381949b0ce
manonfmr <117460396+manonfmr@users.noreply.github.com>
2022-11-04 14:37:26 +0000

    ex2

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Bonjour");
     System.out.println("Famery");
 
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Taper un nombre");
+    int nombreEntree = scanner.nextInt();
+    for (int i=1; i<11;i++){
+      System.out.println(nombreEntree*i);
+    }
+    scanner.close();
     
   }
 }
